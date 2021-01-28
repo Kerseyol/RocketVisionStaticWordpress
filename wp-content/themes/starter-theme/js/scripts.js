fe19db7cfa0962c949d5c0e19c1fc32380a2d221
Oliver Kersey <oliverjkersey@gmail.com>
2021-01-28 17:06:49 -0500

    initial commit

---
+++ wp-content/themes/starter-theme/js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  $("#wordpress").click(function () {
+    $(".services-text")
+      .text(
+        "Sed nec nibh vel nibh pharetra aliquam vitae in ipsum. Aliquam et felis non purus imperdiet mollis. Etiam sit amet luctus nisi, pellentesque iaculis justo. Phasellus commodo velit neque, et hendrerit tellus tempus nec. Aenean scelerisque aliquet dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis eros eget arcu dictum accumsan. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce non quam ultricies dui malesuada pulvinar at a quam. Aenean tempus eu lorem sit amet volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras laoreet dolor non lacus dictum tempor. Aliquam sagittis consequat purus eget porta. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut euismod feugiat metus, eget faucibus odio pulvinar vel. Cras auctor felis eros, in faucibus nisl placerat sed. Nulla sit amet magna in orci hendrerit ullamcorper eget at ipsum. Cras pretium diam eros, vel mattis mauris bibendum ut. Ut nec ligula purus. Phasellus eu lorem ornare, accumsan metus a, molestie odio. Maecenas dignissim facilisis lobortis. "
+      )
+      .fadeIn(400);
+  });
+});
+$(document).ready(function () {
+  $("#digital").click(function () {
+    $(".services-text")
+      .text(
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, omnis laboriosam, a magni iusto placeat inventore enim quasi ducimus fugit fuga consequatur tempore eveniet esse beatae tempora cumque possimus blanditiis sapiente delectus, nihil alias fugiat vero. In, natus laboriosam! Adipisci ratione beatae dolorem, neque dolores sunt culpa quas reprehenderit nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis temporibus quidem neque aspernatur qui quaerat voluptates eum quia consequatur consectetur. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam placeat amet error vitae similique iste quis expedita quaerat, debitis tenetur. "
+      )
+      .fadeIn(400);
+  });
+});
+$(document).ready(function () {
+  $("#shopify").click(function () {
+    $(".services-text")
+      .text(
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero rerum commodi quas sint, ipsum odit, reprehenderit voluptas harum maiores similique alias nemo. Cupiditate porro est voluptas, magni molestias consectetur ut eius nobis dolor deserunt, impedit et fugit error atque optio sequi. Nesciunt sunt, error deleniti, maiores totam, odio eos nemo quaerat nam provident eveniet vero adipisci libero. Delectus ratione soluta eum saepe, veritatis iusto harum quis. Vero repellendus quibusdam laudantium.In, natus laboriosam! Adipisci ratione beatae dolorem, neque dolores sunt culpa quas reprehenderit nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis temporibus quidem neque aspernatur qui quaerat voluptates eum quia consequatur consectetur. "
+      )
+      .fadeIn(400);
+  });
+});
+
+$("#show-menu").click(function () {
+  $("#menu-main-menu").toggle(100);
+});
+
+$("#menu-blog-menu").click(function () {
+  $(".sub-menu").toggle(200);
+});
